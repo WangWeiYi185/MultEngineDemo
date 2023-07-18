e88057c20cfce0571a3a7c2dca09fd30ae0a05d3
weiyi.wang <weiyi.wang@ly.com>
2023-07-18 16:23:40 +0800

    feat: 添加for web 逻辑

--- EnginePJ/EnginePJ/YCWebContainer.swift
+++ EnginePJ/EnginePJ/YCWebContainer.swift
@@ -9,23 +9,35 @@ import UIKit
 import WebKit
 
 
-class YCWebContainer: UIViewController, WKUIDelegate {
+class YCWebContainer: UIViewController, WKUIDelegate, WKNavigationDelegate {
     
     var webView: WKWebView!
     
 
     override func loadView() {
         let webConfiguration = WKWebViewConfiguration()
-        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+
+        webConfiguration.preferences.setValue(true, forKey: "allowFileAccessFromFileURLs")
+        webView = WKWebView(frame: CGRect(x: 0, y: 0, width: 100, height: 100), configuration: webConfiguration)
+        webView.navigationDelegate = self
         webView.uiDelegate = self
         view = webView
     }
+    
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        
+    }
+    
+
+    
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let myURL = URL(string:"localhost:8080")
+        // 解决本地 1004问题 两个怀疑方向， 1:跨域 2:https
+        let myURL = URL(string:"http://10.181.141.192")
+//        let myURL = URL(string:"http://localhost:8080")
         let myRequest = URLRequest(url: myURL!)
         webView.load(myRequest)
     }
--- engine_web/lib/package_mine.dart
+++ engine_web/lib/package_mine.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:engine_web/navigator_imp.dart';
+import 'package:flutter/services.dart';
 
 class PackageMine extends StatefulWidget {
-  const PackageMine({ Key? key }) : super(key: key);
+  const PackageMine({Key? key}) : super(key: key);
 
   @override
   PackageMineState createState() => PackageMineState();
@@ -15,21 +16,22 @@ class PackageMineState extends State<PackageMine> {
     return Material(
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('我的'),
-           leading: IconButton(
-            icon: const Icon(Icons.arrow_back_ios),
-            onPressed: () {
-              YCNavigator().pop();
-            },
-        ),
-          actions: [
-            TextButton(
-              child: Text(text!),
-            onPressed: () {},
-            )
-        ]
-        ),
-
+            title: const Text('我的'),
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              onPressed: () {
+                YCNavigator().pop();
+              },
+            ),
+            actions: [
+              TextButton(
+                child: const Text("快乐星球"),
+                onPressed: () {
+                  const bkupChannel = MethodChannel('disaster-backUp');
+                  bkupChannel.invokeMethod<void>("disasterBackUp", {});
+                },
+              )
+            ]),
         body: const Center(
           child: Text('我的'),
         ),
